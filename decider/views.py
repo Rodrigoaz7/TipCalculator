481c1006b138921426cb339fc36b53cc5911bd1a
rodrigo <rodrigo.aze7@gmail.com>
2018-08-22 16:49:32 -0300

    #02: Template feito e api terminada

--- decider/views.py
+++ decider/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from rest_framework import viewsets, generics, permissions
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 from .fuzzifier import *
 
+class TemplateView(TemplateView):
+	template_name = "templates/index.html"
+
+
 class DeciderView(generics.ListCreateAPIView):
 
 	permission_classes = (AllowAny,)
 	
 	def get(self, request, format=None):
-		return Response('API DIABETES IA', status=status.HTTP_200_OK)
+		return Response('API TIP CALCULATOR FUZZY', status=status.HTTP_200_OK)
 
 	def post(self, request, format=None):
-		resposta = fuzzifier(request.data['nota_qualidade'], request.data['nota_servico'])
-		return Response(resposta, status=status.HTTP_200_OK)
+
+		if(request.data.get('nota_qualidade') and request.data.get('nota_servico')):
+			resposta = fuzzifier(request.data['nota_qualidade'], request.data['nota_servico'])
+			return Response(resposta, status=status.HTTP_200_OK)
+		return Response("ERRO do JSON", status=status.HTTP_400_BAD_REQUEST)
--- diabetesmedidor/urls.py
+++ diabetesmedidor/urls.py
@@ -1,11 +1,12 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from decider.views import DeciderView
+from decider.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', view=DeciderView.as_view(), name='decider'),
+    url(r'^decider$', view=DeciderView.as_view(), name='decider'),
+    url(r'^$', view=TemplateView.as_view(), name='tview'),
 ]
